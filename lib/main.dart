92f9e083f5c7f008c8d527dc8695f3c6c1ff3f02
umurzakov-surf <umurzakov@surfstudio.ru>
2021-12-22 15:31:40 +0600

    added analizator

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation/ui/navigation_app.dart';
+
+void main() {
+  runApp(const NavigationApp());
+}
---
+++ lib/ui/navigation_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation/ui/home/home_page.dart';
+
+class NavigationApp extends StatelessWidget {
+  const NavigationApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
